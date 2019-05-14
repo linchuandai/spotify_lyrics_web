22cb57a26cd48b96bf874057ca0a8d18b1389025
Leon Dai <linchuandai@gmail.com>
2019-05-13 23:30:53 -0400

    Updated scraping to use Genius API

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -5,18 +5,7 @@ from datetime import datetime
 import time
 import json
 
-
 def scrape (song_name, song_artist):
-
-    lyricheaders = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'DNT': '1',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-    }
 
     # used only for testing so we don't continually have to call spotify api
     if not song_name and not song_artist:
@@ -27,27 +16,29 @@ def scrape (song_name, song_artist):
         query = f"{song_name} {song_artist} lyrics"
         lyrics = f"{song_name} {song_artist}"
 
-    url = f'https://www.google.com/search?q={query}&ie=utf-8&oe=utf-8'
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + 'O-a11WJg5nnCfmtkbvPxWO1Z7d7Q8a2YbEZg0RG7nVfW859_JuHAs9wWFk3uQNDt'}
+    search_url = base_url + '/search'
+    data = {'q': song_name + ' ' + song_artist}
+    response = requests.get(search_url, data=data, headers=headers)
 
-    request = requests.get(url, headers=lyricheaders)
+    json = response.json()
+    remote_song_info = None
 
-    lyricsArr = [lyrics]
+    for hit in json['response']['hits']:
+        if song_artist.lower() in hit['result']['primary_artist']['name'].lower():
+            remote_song_info = hit
+            break
 
-    lyrics_paragraph = BeautifulSoup(request.text, "html.parser").find_all("div", {"jsname": "U8S5sf"})
+    if remote_song_info:
+        song_url = remote_song_info['result']['url']
 
-    for para in lyrics_paragraph:
-        # TODO: find better way of removing the partially cut off paragraph
-        if BeautifulSoup(str(para), "html.parser").find("div", {"class": "OULBYb"}) is not None:
-            continue
+    page = requests.get(song_url)
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics_paragraph = html.find('div', class_='lyrics').get_text()
 
-        lyrics_line = BeautifulSoup(str(para), "html.parser").find_all("span", {"jsname": "YS01Ge"})
-
-        for line in lyrics_line:
-            lyrics = f"{lyrics}<br>{line.text}"
-            lyricsArr.append(line.text)
-
-        lyrics = f"{lyrics} <br>"
-        lyricsArr.append('')
+    lyricsArr = lyrics_paragraph.split('\n')
+    lyricsArr[0] = lyrics
 
     return lyricsArr
 
@@ -74,36 +65,38 @@ def getSong(token):
     return scrape(song, artist)
 
 # here for testing this function with terminal, probably remove later
-# def getSongWithUser(user, passw):
-#     token = None
-#     if token is None or token_exp < datetime.now():
-#         data = st.start_session(user, passw)
-#         token = data[0]
-#         token_exp = data[1]
+def getSongWithUser(user, passw):
+    token = None
+    if token is None or token_exp < datetime.now():
+        data = st.start_session(user, passw)
+        token = data[0]
+        token_exp = data[1]
 
-#     spotheaders = {'Accept': 'application/json',
-#         'Content-Type': 'application/json',
-#         'Authorization': f'Bearer {token}',
-#     }
+    spotheaders = {'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+    }
 
-#     artist = ""
-#     song = ""
+    artist = ""
+    song = ""
 
-#     response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=spotheaders)
-#     if response.status_code != 200:
-#         print(response.status_code)
-#         print("Bad Response from Spotify API")
+    # TODO: Handle more errors (such as 204)
+    response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=spotheaders)
+    if response.status_code != 200:
+        print(response.status_code)
+        print("Bad Response from Spotify API")
 
-#     else:
-#         json_data = json.loads(response.text)
-#         artist = json_data["item"]["artists"][0]["name"]
-#         if song != json_data["item"]["name"]:
-#             song = json_data["item"]["name"]
+    else:
+        json_data = json.loads(response.text)
+        artist = json_data["item"]["artists"][0]["name"]
+        if song != json_data["item"]["name"]:
+            song = json_data["item"]["name"]
 
-#     return scrape(song, artist)
+    return scrape(song, artist)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     while True:
-#         print(getSongWithUser('', ''))
-#         time.sleep(3)
+    # while True:
+    for line in scrape('Payphone', 'Maroon 5'):
+        print(line)
+        # time.sleep(3)
