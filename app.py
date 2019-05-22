a806d1f64e147beb9ec801983d4848992b2e212a
Leon Dai <linchuandai@gmail.com>
2019-05-21 22:30:48 -0400

    Separated home and login

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
     if request.method == 'POST':
         username =  request.form['username']
         password = request.form['password']
@@ -18,7 +22,7 @@ def home():
         session['token_exp'] = data[1]
         return redirect(url_for('lyrics'))
 
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/lyrics')
 def lyrics():
